35b004d04d073092af2b71bf3ddda8ecef15a7f3
Vincent FACK <vincent.fack@etudiant.univ-lille1.fr>
2015-02-04 09:50:48 +0100

    Le serveur ne s'arrete plus

--- webserver/main.c
+++ webserver/main.c
@@ -10,26 +10,35 @@ if (argc > 1 && strcmp(argv[1], " - advice " ) == 0) {
 	printf ("Need an advice ?\n");
 	return 0;
 }*/
-int socket_client;
 char *buf = malloc(50);
 int nb_char;
 int socket_serveur = creer_serveur(8080);
-socket_client = accept(socket_serveur,NULL,NULL);
-if(socket_client == -1) {
-	perror("accept");
-	return -1;
-}
 
-	const char *message_bienvenue = "Bonjour, bienvenue sur mon serveur\n";
-	sleep(1);
-	write(socket_client, message_bienvenue, strlen(message_bienvenue));
-	while(1) {
-		nb_char = read(socket_client, buf, 50);
-		if( nb_char == -1){
-			perror("read");
+
+	while(1){
+		int socket_client;
+		socket_client = accept(socket_serveur,NULL,NULL);
+		if(socket_client == -1) {
+			perror("accept");
 			return -1;
 		}
-		write(socket_client, buf, nb_char);
+
+			const char *message_bienvenue = "Bonjour, bienvenue sur mon serveur\n";
+			sleep(1);
+			write(socket_client, message_bienvenue, strlen(message_bienvenue));
+			while(1) {
+				nb_char = read(socket_client, buf, 50);
+				if( nb_char == -1){
+					perror("read");
+					return -1;
+				}
+				if( nb_char == 0){
+					break;
+				}
+				write(socket_client, buf, nb_char);
+			}
+		close(socket_client);
+		
 	}
 	return 0;
 }
